197e6a021e1cbc231c6485c0aa7bc35107783155
Sebibebi67 <sebibebi67@gmail.com>
2019-11-14 16:17:20 +0100

    getEnergy and getGhosts

--- player.c
+++ player.c
@@ -26,52 +26,106 @@ extern const int ENERGY_SCORE; // reward for eating an energizer
 const char * binome="Sébastien HERT & Tony CHOUTEAU";
 
 // put the prototypes of your additional functions/procedures below
-
+direction getEnergy(char ** map,int xsize, int ysize, int x, int y);
+direction getGhosts(char ** map,int xsize, int ysize, int x, int y);
 
 // change the pacman function below to build your own player
 // your new pacman function can use as many additional functions/procedures as needed; put the code of these functions/procedures *AFTER* the pacman function
 direction pacman(
-		 char * * map, // the map as a dynamic array of strings, ie of arrays of chars
-		 int xsize, // number of columns of the map
-		 int ysize, // number of lines of the map
-		 int x, // x-position of pacman in the map 
-		 int y, // y-position of pacman in the map
-		 direction lastdirection, // last move made by pacman (see pacman.h for the direction type; lastdirection value is -1 at the beginning of the game
-		 bool energy, // is pacman in energy mode? 
-		 int remainingenergymoderounds // number of remaining rounds in energy mode, if energy mode is true
-		 ) {
-  direction d; // the direction to return
-  bool north=false; // indicate whether pacman can go north; no by default
-  bool east=false; // indicate whether pacman can go east; no by default
-  bool south=false; // indicate whether pacman can go south; no by default
-  bool west=false; // indicate whether pacman can go west; no by default
-  bool ok=false; // turn true when a valid direction is randomly chosen
+			char * * map, // the map as a dynamic array of strings, ie of arrays of chars
+			int xsize, // number of columns of the map
+			int ysize, // number of lines of the map
+			int x, // x-position of pacman in the map 
+			int y, // y-position of pacman in the map
+			direction lastdirection, // last move made by pacman (see pacman.h for the direction type; lastdirection value is -1 at the beginning of the game
+			bool energy, // is pacman in energy mode? 
+			int remainingenergymoderounds // number of remaining rounds in energy mode, if energy mode is true
+			) {
+	direction d; // the direction to return
 
-  // can pacman go north?
-  if(y==0 || (y>0 && map[y-1][x]!=WALL && map[y-1][x]!=DOOR)) north=true;
-  // can pacman go east?
-  if(x==xsize-1 || (x<xsize-1 && map[y][x+1]!=WALL && map[y][x+1]!=DOOR)) east=true;
-  // can pacman go south?
-  if(y==ysize-1 || (y<ysize-1 && map[y+1][x]!=WALL && map[y+1][x]!=DOOR)) south=true;
-  // can pacman go west?
-  if(x==0 || (x>0 && map[y][x-1]!=WALL && map[y][x-1]!=DOOR)) west=true;
+	//First, we should get super powers
+	if (!energy || (energy && remainingenergymoderounds<5))
+	{
+		d = getEnergy(map, xsize, ysize, x, y);
+	}
 
-  // guess a direction among the allowed four, until a valid choice is made
-  do {
-    d=rand()%4;
-    if((d==NORTH && north)
-       || (d==EAST && east)
-       || (d==SOUTH && south)
-       || (d==WEST && west)) {
-      ok=true;
-    }  
-  } while(!ok);
+	//Now we have super Powers, we should find the best way to find Ghosts
+	else {
+		d = getGhosts(map, xsize, ysize, x, y);
+	}
+	
+
+
+
+
+
+
+
+
+//   bool north=false; // indicate whether pacman can go north; no by default
+//   bool east=false; // indicate whether pacman can go east; no by default
+//   bool south=false; // indicate whether pacman can go south; no by default
+//   bool west=false; // indicate whether pacman can go west; no by default
+//   bool ok=false; // turn true when a valid direction is randomly chosen
+
+//   // can pacman go north?
+//   if(y==0 || (y>0 && map[y-1][x]!=WALL && map[y-1][x]!=DOOR)) north=true;
+//   // can pacman go east?
+//   if(x==xsize-1 || (x<xsize-1 && map[y][x+1]!=WALL && map[y][x+1]!=DOOR)) east=true;
+//   // can pacman go south?
+//   if(y==ysize-1 || (y<ysize-1 && map[y+1][x]!=WALL && map[y+1][x]!=DOOR)) south=true;
+//   // can pacman go west?
+//   if(x==0 || (x>0 && map[y][x-1]!=WALL && map[y][x-1]!=DOOR)) west=true;
+
+//   // guess a direction among the allowed four, until a valid choice is made
+//   do {
+// 	d=rand()%4;
+// 	if((d==NORTH && north)
+// 	   || (d==EAST && east)
+// 	   || (d==SOUTH && south)
+// 	   || (d==WEST && west)) {
+// 	  ok=true;
+// 	}  
+//   } while(!ok);
 
   // answer to the game engine 
-  return d;
+	return d;
 }
 
 // the code of your additional functions/procedures must be put below
 
+direction getEnergy(char ** map,int xsize, int ysize, int x, int y){
+	direction d;
+	bool north=false; // indicate whether pacman can go north; no by default
+  	bool east=false; // indicate whether pacman can go east; no by default
+  	bool south=false; // indicate whether pacman can go south; no by default
+	bool west=false; // indicate whether pacman can go west; no by default
+	bool ok=false; // turn true when a valid direction is randomly chosen
+
+	// can pacman go north?
+	if(y==0 || (y>0 && map[y-1][x]!=WALL && map[y-1][x]!=DOOR)) north=true;
+	// can pacman go east?
+	if(x==xsize-1 || (x<xsize-1 && map[y][x+1]!=WALL && map[y][x+1]!=DOOR)) east=true;
+	// can pacman go south?
+	if(y==ysize-1 || (y<ysize-1 && map[y+1][x]!=WALL && map[y+1][x]!=DOOR)) south=true;
+	// can pacman go west?
+	if(x==0 || (x>0 && map[y][x-1]!=WALL && map[y][x-1]!=DOOR)) west=true;
+
+	// guess a direction among the allowed four, until a valid choice is made
+	do {
+		d=rand()%4;
+		if((d==NORTH && north)
+		|| (d==EAST && east)
+		|| (d==SOUTH && south)
+		|| (d==WEST && west)) {
+		ok=true;
+			}  
+  	} while(!ok);
+	return d;
+}
+
+direction getEnergy(char ** map,int xsize, int ysize, int x, int y){
+	return NORTH;
+}
 
 
